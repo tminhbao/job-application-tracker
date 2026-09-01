96b0d63cda07d976b7ae040e34889eea045d3680
tminhbao <tminhbao2001@gmail.com>
2026-09-01 17:10:03 +0700

    JAT-001: fix cannot connect to DB

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,18 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { initializeUserBoard } from "../init-user-board";
 import connectDB from "../db";
+import { MongoClient } from "mongodb";
 
-const mongooseInstance = await connectDB();
-const client = mongooseInstance.connection.getClient();
+// const mongooseInstance = await connectDB();
+// const client = mongooseInstance.connection.getClient();
+// const db = client.db();
+
+const client = new MongoClient(process.env.MONGODB_URI as string);
 const db = client.db();
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db as any, {
-    client: client as any,
+  database: mongodbAdapter(db, {
+    client: client,
   }),
   session: {
     cookieCache: {
